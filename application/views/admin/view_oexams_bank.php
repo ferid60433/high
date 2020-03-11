e07003910ea5f889bc9d5eed1220202cb3642ca3
Adedoyin Michael Adekoniye <michaeladekoniye@gmail.com>
2020-03-11 11:48:07 +0100

    a

--- application/views/admin/view_oexams_bank.php
+++ application/views/admin/view_oexams_bank.php
@@ -1,15 +1,31 @@
 <?php $this->load->view("admin/inc/header") ?>
 <style>
-    div.checker, div.radio {
+    div.checker,
+    div.radio {
         vertical-align: unset !important;
     }
 
-    div.checker span, div.radio span {
+    div.checker span,
+    div.radio span {
         background-position: unset !important;
     }
 
     .card-body .row {
         margin-top: 10px;
+    }
+    .qNum{
+        background: #2c7be5;
+        border-radius: 100%;
+        width:30px;
+        height:30px;
+        text-align:center;
+        color:#fff;
+        font-weight:bold;
+        padding-top:4px;
+    }
+    .qPhrase{
+        padding:2px 5px 2px;
+        background: #cecefe;
     }
 </style>
 <div class="row">
@@ -62,6 +78,30 @@
                     </div>
                     <div class="col-sm-6">
                         <input type="radio" name="single"> 4
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-12">
+        <div class="card card-white">
+            <div class="card-heading clearfix row">
+                <h4 class="col-md-7">What is Biology?</h4>
+                <span class="col-md-5 text-right" style="font-weight:500">5 Marks</span>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="row">
+                            <div class="col-sm-1">
+                                <div class="qNum">1</div>
+                            </div>
+                            <div class="col-sm-11">
+                                <h5 class="qPhrase">The study of life</h5>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
