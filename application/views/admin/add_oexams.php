e076980b736ea9ccb9b28138c8c177bd174c7965
Adedoyin Michael Adekoniye <michaeladekoniye@gmail.com>
2020-03-12 09:39:10 +0100

    a

---
+++ application/views/admin/add_oexams.php
@@ -0,0 +1,201 @@
+<?php $this->load->view("admin/inc/header") ?>
+<style>
+    .input-group-prepend,
+    .input-group-text {
+        width: 44px !important;
+    }
+</style>
+<div class="row">
+    <div class="col-md-12">
+        <div class="card card-white">
+            <div class="card-body">
+                <div id="rootWizardStudent">
+                    <form id="wizardFormStudent">
+                        <div class="tab-content" id="myTabContent">
+                            <div class="tab-pane fade show active" id="subject" role="tabpanel">
+                                <div class="row m-b-lg">
+                                    <div class="col-md-12">
+                                        <div class="form-group row">
+                                            <label class="col-sm-2 col-form-label">Exam Title</label>
+                                            <div class="col-sm-10">
+                                                <div class="input-group mb-3">
+                                                    <div class="input-group-prepend">
+                                                        <div class="input-group-text"><i class="fas fa-book"></i></div>
+                                                    </div>
+                                                    <input type="text" class="form-control" placeholder="Exam Title">
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label class="col-sm-2 col-form-label">Description</label>
+                                            <div class="col-sm-10">
+                                                <div class="input-group mb-3">
+                                                    <div class="input-group-prepend">
+                                                        <div class="input-group-text"><i class="fas fa-book"></i></div>
+                                                    </div>
+                                                    <textarea type="text" class="form-control" placeholder="Description"></textarea>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label class="col-sm-2 col-form-label">Class</label>
+                                            <div class="col-sm-10">
+                                                <div class="input-group mb-3">
+                                                    <div class="input-group-prepend">
+                                                        <div class="input-group-text"><i class="fas fa-school"></i></div>
+                                                    </div>
+                                                    <select class="form-control" id="class" name="class">
+                                                        <option>Select</option>
+                                                        <option value="bus assistant">JSS1</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label class="col-sm-2 col-form-label">Section</label>
+                                            <div class="col-sm-10">
+                                                <div class="input-group mb-3">
+                                                    <div class="input-group-prepend">
+                                                        <div class="input-group-text"><i class="fas fa-school"></i></div>
+                                                    </div>
+                                                    <select class="form-control" id="section" name="section">
+                                                        <option>Select</option>
+                                                        <option value="bus assistant">JSS1</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label class="col-sm-2 col-form-label">Subject</label>
+                                            <div class="col-sm-10">
+                                                <div class="input-group mb-3">
+                                                    <div class="input-group-prepend">
+                                                        <div class="input-group-text"><i class="fas fa-school"></i></div>
+                                                    </div>
+                                                    <select class="form-control" id="subject" name="subject">
+                                                        <option>Select</option>
+                                                        <option value="bus assistant">English</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label class="col-sm-2 col-form-label">Instruction</label>
+                                            <div class="col-sm-10">
+                                                <div class="input-group mb-3">
+                                                    <div class="input-group-prepend">
+                                                        <div class="input-group-text"><i class="fas fa-book"></i></div>
+                                                    </div>
+                                                    <select class="form-control" id="Instruction" name="Instruction">
+                                                        <option>Select</option>
+                                                        <option value="bus assistant">Answer All</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label class="col-sm-2 col-form-label">Exam Status</label>
+                                            <div class="col-sm-10">
+                                                <div class="input-group mb-3">
+                                                    <div class="input-group-prepend">
+                                                        <div class="input-group-text"><i class="fas fa-list"></i></div>
+                                                    </div>
+                                                    <select class="form-control" id="Instruction" name="Instruction">
+                                                        <option>Select</option>
+                                                        <option value="bus assistant">One Time</option>
+                                                        <option value="bus assistant">Multiple Times</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label class="col-sm-2 col-form-label">Exam Type</label>
+                                            <div class="col-sm-10">
+                                                <div class="input-group mb-3">
+                                                    <div class="input-group-prepend">
+                                                        <div class="input-group-text"><i class="fas fa-list"></i></div>
+                                                    </div>
+                                                    <select class="form-control" id="exam_type">
+                                                        <option>Select</option>
+                                                        <option value="1">Only Duration</option>
+                                                        <option value="2">Date and Duration</option>
+                                                        <option value="3">Date, Time and Duration</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label class="col-sm-2 col-form-label">Duration (in minutes)</label>
+                                            <div class="col-sm-10">
+                                                <div class="input-group mb-3">
+                                                    <div class="input-group-prepend">
+                                                        <div class="input-group-text"><i class="fas fa-clock"></i></div>
+                                                    </div>
+                                                    <input type="text" class="form-control" placeholder="Duration">
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div id="date_div" style="display: none;">
+                                            <div class="form-group row">
+                                                <label class="col-sm-2 col-form-label">Date</label>
+                                                <div class="col-sm-10">
+                                                    <div class="input-group mb-3">
+                                                        <div class="input-group-prepend">
+                                                            <div class="input-group-text"><i class="fas fa-calendar"></i></div>
+                                                        </div>
+                                                        <input type="text" class="form-control date-picker" placeholder="Exam Date">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div id="time_div" style="display: none;">
+                                            <div class="form-group row">
+                                                <label class="col-sm-2 col-form-label">Time From</label>
+                                                <div class="col-sm-10">
+                                                    <div class="input-group mb-3">
+                                                        <div class="input-group-prepend">
+                                                            <div class="input-group-text"><i class="fas fa-clock"></i></div>
+                                                        </div>
+                                                        <input type="text" class="form-control time-picker" placeholder="10:00 AM">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="form-group row">
+                                                <label class="col-sm-2 col-form-label">Time To</label>
+                                                <div class="col-sm-10">
+                                                    <div class="input-group mb-3">
+                                                        <div class="input-group-prepend">
+                                                            <div class="input-group-text"><i class="fas fa-clock"></i></div>
+                                                        </div>
+                                                        <input type="text" class="form-control time-picker" placeholder="10:45 AM">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label class="col-sm-2 col-form-label">Room</label>
+                                            <div class="col-sm-10">
+                                                <div class="input-group mb-3">
+                                                    <div class="input-group-prepend">
+                                                        <div class="input-group-text"><i class="fas fa-school"></i></div>
+                                                    </div>
+                                                    <input type="text" class="form-control" placeholder="Classroom">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <button type="submit" class="finish float-right btn btn-primary btn-lg">Finish</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php $this->load->view("admin/inc/footer") ?>
