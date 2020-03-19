b582e7454223bc8c7257472caee41540d68bd9bb
Adedoyin Michael Adekoniye <michaeladekoniye@gmail.com>
2020-03-19 12:34:45 +0100

    assets purchase on the way, working on assets assignment simulteneously

--- application/views/admin/add_assets_purchase.php
+++ application/views/admin/add_assets_purchase.php
@@ -18,96 +18,112 @@
                                 <div class="row m-b-lg">
                                     <div class="col-md-12">
                                         <div class="form-group row">
-                                            <label class="col-sm-2 col-form-label">Serial</label>
-                                            <div class="col-sm-10">
-                                                <div class="input-group mb-3">
-                                                    <div class="input-group-prepend">
-                                                        <div class="input-group-text"><i class="fas fa-list"></i></div>
-                                                    </div>
-                                                    <input type="text" class="form-control" placeholder="Serial Number" id="fullName" name="fullName">
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label class="col-sm-2 col-form-label">Title</label>
+                                            <label class="col-sm-2 col-form-label">Asset</label>
                                             <div class="col-sm-10">
                                                 <div class="input-group mb-3">
                                                     <div class="input-group-prepend">
                                                         <div class="input-group-text"><i class="fas fa-briefcase"></i></div>
                                                     </div>
-                                                    <input type="text" class="form-control" placeholder="Title" id="fullName" name="fullName">
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label class="col-sm-2 col-form-label">Status</label>
-                                            <div class="col-sm-10">
-                                                <div class="input-group mb-3">
-                                                    <div class="input-group-prepend">
-                                                        <div class="input-group-text"><i class="fas fa-globe"></i></div>
-                                                    </div>
-                                                    <select class="form-control" id="status" name="status">
+                                                    <select class="form-control" id="asset" name="asset">
                                                         <option>Select</option>
-                                                        <option value="">Checked Out</option>
-                                                        <option value="">In Storage</option>
                                                     </select>
                                                 </div>
                                             </div>
                                         </div>
                                         <div class="form-group row">
-                                            <label class="col-sm-2 col-form-label">Condition</label>
+                                            <label class="col-sm-2 col-form-label">Vendor</label>
                                             <div class="col-sm-10">
                                                 <div class="input-group mb-3">
                                                     <div class="input-group-prepend">
-                                                        <div class="input-group-text"><i class="fas fa-school"></i></div>
+                                                        <div class="input-group-text"><i class="fas fa-user"></i></div>
                                                     </div>
-                                                    <select class="form-control" id="condition" name="condition">
+                                                    <select class="form-control" id="Vendor" name="Vendor">
                                                         <option>Select</option>
-                                                        <option value="">New</option>
-                                                        <option value="">Used</option>
                                                     </select>
                                                 </div>
                                             </div>
                                         </div>
                                         <div class="form-group row">
-                                            <label class="col-sm-2 col-form-label">Category</label>
+                                            <label class="col-sm-2 col-form-label">Purchased By</label>
                                             <div class="col-sm-10">
                                                 <div class="input-group mb-3">
                                                     <div class="input-group-prepend">
-                                                        <div class="input-group-text"><i class="fas fa-school"></i></div>
+                                                        <div class="input-group-text"><i class="fas fa-user"></i></div>
                                                     </div>
-                                                    <select class="form-control" id="subject" name="subject">
+                                                    <select class="form-control" id="PurchasedBy" name="PurchasedBy">
                                                         <option>Select</option>
-                                                        <option value="">Computers</option>
                                                     </select>
                                                 </div>
                                             </div>
                                         </div>
                                         <div class="form-group row">
-                                            <label class="col-sm-2 col-form-label">Location</label>
+                                            <label class="col-sm-2 col-form-label">Quantity</label>
                                             <div class="col-sm-10">
                                                 <div class="input-group mb-3">
                                                     <div class="input-group-prepend">
-                                                        <div class="input-group-text"><i class="fas fa-school"></i></div>
+                                                        <div class="input-group-text"><i class="fas fa-list"></i></div>
                                                     </div>
-                                                    <select class="form-control" id="subject" name="subject">
+                                                    <input type="text" class="form-control" placeholder="Quantity" >
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label class="col-sm-2 col-form-label">Unit</label>
+                                            <div class="col-sm-10">
+                                                <div class="input-group mb-3">
+                                                    <div class="input-group-prepend">
+                                                        <div class="input-group-text"><i class="fas fa-sort"></i></div>
+                                                    </div>
+                                                    <select class="form-control" id="unit" name="unit">
                                                         <option>Select</option>
-                                                        <option value="">Akwa Ibom</option>
+                                                        <option value="Kg">Kg</option>
+                                                        <option value="Piece">Piece</option>
+                                                        <option value="Other">Other</option>
                                                     </select>
                                                 </div>
                                             </div>
                                         </div>
                                         <div class="form-group row">
-                                            <label class="col-sm-2 col-form-label">Attachment</label>
+                                            <label class="col-sm-2 col-form-label">Purchase Price</label>
                                             <div class="col-sm-10">
                                                 <div class="input-group mb-3">
                                                     <div class="input-group-prepend">
-                                                        <div class="input-group-text"><i class="fas fa-camera"></i></div>
+                                                        <div class="input-group-text"><i class="fas fa-credit-card"></i></div>
                                                     </div>
-                                                    <div class="custom-file">
-                                                        <input type="file" class="custom-file-input" id="customFile">
-                                                        <label class="custom-file-label" for="customFile">Choose file</label>
+                                                    <input type="text" class="form-control" placeholder="Purchase Price" >
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label class="col-sm-2 col-form-label">Purchase Date</label>
+                                            <div class="col-sm-10">
+                                                <div class="input-group mb-3">
+                                                    <div class="input-group-prepend">
+                                                        <div class="input-group-text"><i class="fas fa-calendar"></i></div>
                                                     </div>
+                                                    <input type="text" class="form-control date-picker" placeholder="Purchase Date" >
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label class="col-sm-2 col-form-label">Service Date</label>
+                                            <div class="col-sm-10">
+                                                <div class="input-group mb-3">
+                                                    <div class="input-group-prepend">
+                                                        <div class="input-group-text"><i class="fas fa-calendar"></i></div>
+                                                    </div>
+                                                    <input type="text" class="form-control date-picker" placeholder="Service Date" >
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label class="col-sm-2 col-form-label">Expire Date</label>
+                                            <div class="col-sm-10">
+                                                <div class="input-group mb-3">
+                                                    <div class="input-group-prepend">
+                                                        <div class="input-group-text"><i class="fas fa-calendar"></i></div>
+                                                    </div>
+                                                    <input type="text" class="form-control date-picker" placeholder="Expire Date" >
                                                 </div>
                                             </div>
                                         </div>
---
+++ application/views/admin/assets_assignment.php
@@ -0,0 +1,41 @@
+<?php $this->load->view("admin/inc/header") ?>
+<?php $this->load->view("inc/post-meta") ?>
+<?php $this->load->view("admin/inc/pre-body") ?>
+<div class="row">
+    <div class="col-md-12">
+        <div class="card card-white">
+            <div class="card-heading clearfix mb-3">
+                <button type="button" class="btn btn-outline-primary" onclick="window.location='<?=base_url('admin/assets/assignment/add')?>'"><i class="fas fa-plus"></i> Add New</button>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table datatables_init" style="width: 100%;">
+                        <thead>
+                            <tr>
+                                <th>S/N</th>
+                                <th>Asset</th>
+                                <th>Assign Quantity</th>
+                                <th>Role</th>
+                                <th>Checkout To</th>
+                                <th>Due Date</th>
+                                <th>Checkout Date</th>
+                                <th>Checkin Date</th>
+                                <th>Status</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1.</td>
+                                <td>Computers</td>
+                                <td><a class="btn btn-outline-warning" href="<?=base_url("admin/assets/assignment/edit")?>">Edit</a> | <a class="btn btn-outline-danger" href="<?=base_url("admin/assets/assignment/delete")?>">Delete</a></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php $this->load->view("admin/inc/footer") ?>
+<?php $this->load->view("inc/post-script")?>
